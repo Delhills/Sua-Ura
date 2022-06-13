dee36efe0601ec57083b847053c43e4ca508b3d0
zap-zempai <83459658+zap-zempai@users.noreply.github.com>
2022-06-13 18:56:44 +0200

    plataforma amb maxim i minim

--- Clean-lab-proyect-main/Assets/movement.cs
+++ Clean-lab-proyect-main/Assets/movement.cs
@@ -13,20 +13,46 @@ public class movement : MonoBehaviour
 
     public Vector3 dir;
 
+    public Vector3 start;
+    public Vector3 end;
+
     private bool isPlayerOneHere = false;
     private bool isPlayerTwoHere = false;
+
+    private float start_point;
+    private float end_point;
+
+    private float current_point;
+
     //public Vector3 hundirse;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        start_point = Vector3.Dot(dir, start);
+        end_point = Vector3.Dot(dir, end);
     }
 
     void Update()
     {
-        if (isPlayerOneHere && isPlayerTwoHere) { // Si quitais uno de los booleanos entonces la plataforma solo funciona para uno
+
+        current_point = Vector3.Dot(dir, rb.position);
+
+        if (isPlayerOneHere && isPlayerTwoHere && (current_point < end_point)) { // Si quitais uno de los booleanos entonces la plataforma solo funciona para uno
             rb.velocity = dir * speed; // Es una guarrada pero mira asi teneis plataformas de 1 player por si quereis
-        } else {
+        } 
+        else if (!isPlayerOneHere && !isPlayerTwoHere && current_point > start_point) {
+            rb.velocity = -1 * dir * speed;
+        }
+        else if (current_point > end_point) {
+            rb.velocity = Vector3.zero;
+            rb.position = end;
+        }
+        else if (current_point < start_point) {
+            rb.velocity = Vector3.zero;
+            rb.position = start;
+        }
+        else {
             rb.velocity = Vector3.zero;
         }
     }
